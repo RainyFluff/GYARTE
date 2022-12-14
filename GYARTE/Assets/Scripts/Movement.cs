0662b8ce197be2188d840daac632bf155d3e87d8
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2022-12-14 10:00:27 +0100

    Fixed Camera stretch

--- GYARTE/Assets/Scripts/Movement.cs
+++ GYARTE/Assets/Scripts/Movement.cs
@@ -9,7 +9,10 @@ public class Movement : MonoBehaviour
     public float Speed = 10f;
     public float jumpForce = 10f;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public Camera cam;
+=======
+>>>>>>> Stashed changes
     public float sens = 10;
     
     void Start()
