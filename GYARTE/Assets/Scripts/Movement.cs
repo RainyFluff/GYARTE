853bdb1a2aace6bfa5d3ae10a2ba178e53d73694
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2022-12-15 09:23:19 +0100

    Movement with clamp

--- GYARTE/Assets/Scripts/Movement.cs
+++ GYARTE/Assets/Scripts/Movement.cs
@@ -7,7 +7,9 @@ public class Movement : MonoBehaviour
 {
     [Header("General")]
     public Rigidbody rb;
-    public float Speed = 10f;
+    public float speed = 0.2f;
+    public float airSpeed = 0.05f;
+    public float normalSpeed = 0.2f;
     public float jumpForce = 10f;
     public GameObject cam;
     public float sens = 10;
@@ -17,6 +19,7 @@ public class Movement : MonoBehaviour
     public GameObject orientation;
     LayerMask groundmask;
     public Vector3 direction = Vector3.right;
+    public float maxSpeed = 5;
 
     void Start()
     {
@@ -53,28 +56,50 @@ public class Movement : MonoBehaviour
         
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(orientation.transform.right * -4);
+            rb.AddForce(orientation.transform.right * -speed, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(orientation.transform.right * 4);
+            rb.AddForce(orientation.transform.right * speed, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(orientation.transform.forward * 4);
+            rb.AddForce(orientation.transform.forward * speed, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(orientation.transform.forward * -4);
+            rb.AddForce(orientation.transform.forward * -speed, ForceMode.VelocityChange);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+        if (IsGrounded)
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            speed = normalSpeed;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            }
+
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+            }
         }
+        else
+        {
+            speed = airSpeed;
+        }
+
+        
+
+        
+        
+
+
+
+
     }
 }
