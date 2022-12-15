4fb8db2ea74c0c9a9b7e3a3ce1d6279355cca2f4
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2022-12-15 13:12:31 +0100

    Weaponswitch for 3rd weapon

--- GYARTE/Assets/Scripts/Weaponswitch.cs
+++ GYARTE/Assets/Scripts/Weaponswitch.cs
@@ -6,24 +6,36 @@ public class Weaponswitch : MonoBehaviour
 {
     public GameObject Revolver;
     public GameObject Shotgun;
+    public GameObject Katana;
     // Start is called before the first frame update
     void Start()
     {
         Shotgun.SetActive(false);
+        Revolver.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            Katana.SetActive(true);
+            Shotgun.SetActive(false);
+            Revolver.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Katana.SetActive(false);
             Shotgun.SetActive(true);
             Revolver.SetActive(false);
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha1))
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
+            Katana.SetActive(false);
             Shotgun.SetActive(false);
             Revolver.SetActive(true);
         }
+
     }
 }
