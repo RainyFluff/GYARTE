89700c93e5b07f5a67a02d1474dc49ec05a394c8
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2023-01-10 14:40:26 +0100

    Reload

--- GYARTE/Assets/Scripts/Revolver.cs
+++ GYARTE/Assets/Scripts/Revolver.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Revolver : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject bulletSpawn;
+    public int bullets = 6;
+    public float reloadTime = 2;
+    float timer = 10000000;
+    bool isReloading = false;
+    public TextMeshProUGUI bulletText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +22,34 @@ public class Revolver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bulletText.text = bullets.ToString();
+        Debug.Log(bullets);
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
+            if (bullets >= 1 && !isReloading)
+            {
+                Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
+                bullets--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(bullets <= 5)
+            {
+                isReloading = true;
+                timer = Time.time;
+            }
+        }
+
+        if (isReloading)
+        {
+            if((Time.time - timer) > reloadTime)
+            {
+                bullets = 6;
+                isReloading = false;
+                timer = 1000000;
+            } 
         }
         
     }
