b75af8da59cf52eb1d3cc7c719690b08a0c7620b
jakobnystrom04 <jakob.nystrom@elev.ga.lbs.se>
2023-02-05 15:15:07 +0100

    mmmm meny

---
+++ GYARTE/Assets/Scripts/inGameMenu.cs
@@ -0,0 +1,41 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inGameMenu : MonoBehaviour
+{
+    public Canvas canvas;
+    float timeScale;
+    bool active = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.gameObject.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (active == false)
+            {
+                canvas.gameObject.SetActive(true);
+                active = true;
+                Cursor.visible = true;
+            }
+            else
+            {
+                removeMenu();
+                active = false;
+                Cursor.visible = false;
+            }
+        }
+    }
+    public void removeMenu()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+}
